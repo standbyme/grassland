f8a96be57f701bc1e2001a16879e6f86c8ccfb3d
TGS <tgs@tgs.blue>
2018-01-07 17:24:51 +0800

    format file path

--- test/acquire_semaphore.js
+++ test/acquire_semaphore.js
@@ -1,8 +1,13 @@
-const fs = require('fs')
 const Redis = require('ioredis')
 const redis = new Redis(32768, '127.0.0.1')
 
-const config = { acquire_semaphore_lua_script_path: '../src/lua/acquire_semaphore.lua' }
+const fs = require('fs')
+
+const redis_utils = require('../src/redis_utils.js')
+
+const config = {
+    acquire_semaphore_lua_script_path: '../src/lua/acquire_semaphore.lua',
+}
 
 const acquire_semaphore_lua = fs.readFileSync(config.acquire_semaphore_lua_script_path).toString()
 
@@ -12,28 +17,21 @@ redis.defineCommand('acquire_semaphore', {
 })
 
 async function main() {
-    const result = await redis.pipeline()
-        .acquire_semaphore('1', '1', '1', Date.now(), '5', '15000')
-        .acquire_semaphore('2', '1', '1', Date.now(), '5', '15000')
-        .acquire_semaphore('3', '1', '1', Date.now(), '5', '15000')
-        .acquire_semaphore('4', '1', '1', Date.now(), '5', '15000')
-        .acquire_semaphore('7', '1', '1', Date.now(), '5', '15000')
-        .acquire_semaphore('9', '1', '1', Date.now(), '5', '15000')
-        .exec();
+    const result = await redis_utils.acquire_semaphore(redis, '1', '1', '1', '3')
+
+    // redis.pipeline()
+    // .acquire_semaphore('1', '1', '1', Date.now(), '5', '15000')
+    // .acquire_semaphore('2', '1', '1', Date.now(), '5', '15000')
+    // .acquire_semaphore('3', '1', '1', Date.now(), '5', '15000')
+    // .acquire_semaphore('4', '1', '1', Date.now(), '5', '15000')
+    // .acquire_semaphore('7', '1', '1', Date.now(), '5', '15000')
+    // .acquire_semaphore('9', '1', '1', Date.now(), '5', '15000')
+    // .exec();
     console.log(result)
 }
 
 main().then(() => {
     redis.disconnect()
+}).catch((err) => {
+    console.log(err)
 })
-
-
-
-// redis.pipeline()
-//     .echo('5', '1', '1', Date.now(), '3')
-//     .echo('6', '1', '1', Date.now(), '3')
-//     .echo('7', '1', '1', Date.now(), '3')
-//     .echo('8', '1', '1', Date.now(), '3')
-//     .exec(function (err, results) {
-//         console.log(results)
-//     });
