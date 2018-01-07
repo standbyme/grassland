0ffa8e09e42fb2d0298e0684e10d473305450441
TGS <tgs@tgs.blue>
2018-01-07 18:53:45 +0800

    fix typo

--- src/lua/acquire_semaphore.lua
+++ src/lua/acquire_semaphore.lua
@@ -5,16 +5,16 @@ local now = tonumber(ARGV[4])
 local limit = tonumber(ARGV[5])
 local timeout = tonumber(ARGV[6])
 
-local semphore_name = string.format("semphore/%s/%s",project_id,question_id)
+local semaphore_name = string.format("semaphore/%s/%s",project_id,question_id)
 local record_name = string.format("record/%s/%s",project_id,question_id)
 
-redis.call('zadd',semphore_name,now,user_id)
-redis.call('zremrangebyscore',semphore_name,'-inf',now-timeout)
+redis.call('zadd',semaphore_name,now,user_id)
+redis.call('zremrangebyscore',semaphore_name,'-inf',now-timeout)
 
-if((redis.call('zcard',semphore_name)+redis.call('zcard',record_name))<=limit)
+if((redis.call('zcard',semaphore_name)+redis.call('zcard',record_name))<=limit)
 then
     return true
 else
-    redis.call('zrem',semphore_name,user_id)
+    redis.call('zrem',semaphore_name,user_id)
     return false
 end
