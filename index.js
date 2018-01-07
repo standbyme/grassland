7cc23ee525d9b8fdd16606286adb6b16c9d60da5
TGS <tgs@tgs.blue>
2018-01-07 16:50:40 +0800

    move lua script to a single file

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
+const fs = require('fs')
 const Redis = require('ioredis')
 const redis = new Redis(32768, '127.0.0.1')
 
-const acquire_semaphore_lua =
-    `
-    local user_id = ARGV[1]
-    local project_id = ARGV[2]
-    local question_id = ARGV[3]
-    local now = tonumber(ARGV[4])
-    local limit = tonumber(ARGV[5])
-    local timeout = tonumber(ARGV[6])
+const config = { acquire_semaphore_lua_script_path: './acquire_semaphore.lua' }
 
-    local semphore_name = string.format("semphore/%s/%s",project_id,question_id)
-    local record_name = string.format("record/%s/%s",project_id,question_id)
-
-    redis.call('zadd',semphore_name,now,user_id)
-    redis.call('zremrangebyscore',semphore_name,'-inf',now-timeout)
-
-    if((redis.call('zcard',semphore_name)+redis.call('zcard',record_name))<=limit)
-    then
-        return true
-    else
-        redis.call('zrem',semphore_name,user_id)
-        return false
-    end
-    `
+const acquire_semaphore_lua = fs.readFileSync(config.acquire_semaphore_lua_script_path).toString()
 
 redis.defineCommand('acquire_semaphore', {
     numberOfKeys: 0,
     lua: acquire_semaphore_lua
-});
+})
 
 async function main() {
     const result = await redis.pipeline()
-        .acquire_semaphore('1', '1', '1', Date.now(), '5','15000')
-        .acquire_semaphore('2', '1', '1', Date.now(), '5','15000')
-        .acquire_semaphore('3', '1', '1', Date.now(), '5','15000')
-        .acquire_semaphore('4', '1', '1', Date.now(), '5','15000')
-        .acquire_semaphore('7', '1', '1', Date.now(), '5','15000')
-        .acquire_semaphore('9', '1', '1', Date.now(), '5','15000')
+        .acquire_semaphore('1', '1', '1', Date.now(), '5', '15000')
+        .acquire_semaphore('2', '1', '1', Date.now(), '5', '15000')
+        .acquire_semaphore('3', '1', '1', Date.now(), '5', '15000')
+        .acquire_semaphore('4', '1', '1', Date.now(), '5', '15000')
+        .acquire_semaphore('7', '1', '1', Date.now(), '5', '15000')
+        .acquire_semaphore('9', '1', '1', Date.now(), '5', '15000')
         .exec();
     console.log(result)
 }
