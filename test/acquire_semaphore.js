c590a620ed7e86a13fb66688ba87263cff473ff5
TGS <tgs@tgs.blue>
2018-01-07 18:39:08 +0800

    add test case

--- test/acquire_semaphore.js
+++ test/acquire_semaphore.js
@@ -27,6 +27,10 @@ const mock_config = {
     ]
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function main() {
     const result_1 = await Promise.all(
         mock_config
@@ -34,7 +38,57 @@ async function main() {
             .slice(0, 3)
             .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
     )
+    const result_2 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(3, 5)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
+    const result_3 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(0, 3)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
+
+    await sleep(16000)
+
+    const result_4 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(3, 6)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
+
+    const result_5 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(0, 3)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
+
+    await sleep(16000)
+
+    const result_6 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(2, 5)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
+
+    const result_7 = await Promise.all(
+        mock_config
+            .user_ids
+            .slice(0, 1)
+            .map((m) => redis_utils.acquire_semaphore(redis, m, '1', '1', 3))
+    )
     console.log(result_1)
+    console.log(result_2)
+    console.log(result_3)
+    console.log(result_4)
+    console.log(result_5)
+    console.log(result_6)
+    console.log(result_7)
 
 }
 
