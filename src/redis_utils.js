a58f63f617b7cacbecad4544786ae7ae0cbc43f6
TGS <tgs@tgs.blue>
2018-01-08 18:45:34 +0800

    make define_command a function

--- src/redis_utils.js
+++ src/redis_utils.js
@@ -6,7 +6,18 @@ const config = {
         port: 32768
     }
 }
-
+function define_command(redis) {
+    const fs = require('fs')
+    const config = {
+        acquire_question_lua_script_path: './src/lua/acquire_question.lua'
+    }
+    const acquire_question_lua_script = fs.readFileSync(config.acquire_question_lua_script_path).toString()
+    redis.defineCommand('acquire_question', {
+        numberOfKeys: 0,
+        lua: acquire_question_lua_script
+    })
+    return redis
+}
 function raw_connector() {
     const Redis = require('ioredis')
     const redis = new Redis(config.redis.port, config.redis.host)
@@ -15,20 +26,9 @@ function raw_connector() {
 }
 
 function connector() {
-    const fs = require('fs')
-    const config = {
-        acquire_question_lua_script_path: './src/lua/acquire_question.lua'
-    }
-    const acquire_question_lua_script = fs.readFileSync(config.acquire_question_lua_script_path).toString()
-
     const redis = raw_connector()
-
-    redis.defineCommand('acquire_question', {
-        numberOfKeys: 0,
-        lua: acquire_question_lua_script
-    })
-
-    return redis
+    const redis_defined_command = define_command(redis)
+    return redis_defined_command
 }
 
 function acquire_semaphore(redis, user_id, project_id, question_id, limit, timeout = config.semaphore_timeout) {
