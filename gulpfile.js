7d718644666d31401e78741e62fce257561afd18
TGS <tgs@tgs.blue>
2018-01-09 20:55:22 +0800

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+let del = require('del')
+let gulp = require('gulp')
+let ts = require('gulp-typescript')
+let mocha = require('gulp-mocha')
+
+const tsProject = ts.createProject('tsconfig.json')
+
+
+gulp.task('clean', function () {
+    return del(['dist/*'])
+})
+
+gulp.task('tsc', ['clean'], function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest('dist'))
+})
+
+gulp.task('test', function () {
+    return gulp.src(['dist/test/**/**.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec'
+        }))
+})
+
+gulp.task('compile', ['clean', 'tsc'])
