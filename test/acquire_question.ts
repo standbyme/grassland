64cbb3ccacdb79b79b4b8dfbc944e2c1e3679c4a
TGS <tgs@tgs.blue>
2018-01-11 15:07:34 +0800

    remove useless test

--- test/acquire_question.ts
+++ test/acquire_question.ts
@@ -22,6 +22,17 @@ describe('Acquire Question', function () {
             'empty_project_id': '4',
             'project_id': '2'
         }
+        const result = await redis_utils.acquire_question(redis, mock_config.user_id, mock_config.empty_project_id)
+
+        assert.equal(result, null)
+    })
+
+    it('basic test', async function () {
+        const mock_config = {
+            'user_id': '6',
+            'empty_project_id': '4',
+            'project_id': '2'
+        }
         await redis.zadd(`project/${mock_config.project_id}`, '4', '4', '7', '7', '16', '16', '32', '32')
         const result = await redis_utils.acquire_question(redis, mock_config.user_id, mock_config.empty_project_id)
 
