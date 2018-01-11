7cb87b8cdd4524673d53bfa16f472a82c6895aef
TGS <tgs@tgs.blue>
2018-01-11 18:07:35 +0800

    change lock value

--- src/lua/temp_acquire_question.lua
+++ src/lua/temp_acquire_question.lua
@@ -1,6 +1,7 @@
 local key = string.format("lock/%s/%s/%s",user_id,project_id,question_id)
+local value = string.format("%s/%s",project_id,question_id)
 
-redis.call('set', key, 0)
+redis.call('set', key, value)
 redis.call('expire', key, timeout)
 
 return true
--- test/temp_acquire_question.ts
+++ test/temp_acquire_question.ts
@@ -45,7 +45,7 @@ describe('Temp Acquire Question', function () {
         const result_2 = await redis.get(key)
         await redis.disconnect()
 
-        assert.equal(result_1, 0)
+        assert.equal(result_1, `${mock_config.project_id}/${mock_config.question_id}`)
         assert.equal(result_2, null)
     })
 })
